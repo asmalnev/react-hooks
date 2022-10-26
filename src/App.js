30f33fa995baf5db1e3f526a9bb56295086563e0
asmalnev <asmalnev@gmail.com>
2022-10-26 22:51:42 +0300

    119

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-function App() {
+import React from 'react'
+import {Navbar} from './components/Navbar'
+import {Home} from './pages/Home'
+
+export default function App() {
   return (
-    <div className={'container pt-4'}>
-      <h1>Hello world</h1>
-    </div>
+    <React.Fragment>
+      <Navbar/>
+      <div className={'container pt-4'}>
+        <Home/>
+      </div>
+    </React.Fragment>
   )
 }
-
-export default App
